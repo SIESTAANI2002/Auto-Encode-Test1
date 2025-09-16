f6035160f96454b7ecd653cecff426e2e8656002
SIESTAANI2002 <107175442+SIESTAANI2002@users.noreply.github.com>
2025-09-16 06:24:13 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -21,9 +21,9 @@ LOGS = getLogger(__name__)
 load_dotenv('config.env')
 
 ani_cache = {
-    "ongoing": set(),
-    "completed": {},  # ✅ dict instead of set
-    "fetch_animes": True
+    'fetch_animes': True,
+    'ongoing': set(),
+    'completed': set()
 }
 ffpids_cache = list()
 
