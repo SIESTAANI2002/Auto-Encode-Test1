7754bfa1ce5391570f21b3a540894b311ada5727
SIESTAANI2002 <107175442+SIESTAANI2002@users.noreply.github.com>
2025-09-17 20:07:53 +0530

    Update gdrive_uploader.py

--- bot/core/gdrive_uploader.py
+++ bot/core/gdrive_uploader.py
@@ -1,22 +1,24 @@
 import os
 import json
-from pydrive2.auth import GoogleAuth
 from pydrive2.drive import GoogleDrive
+from pydrive2.auth import GoogleAuth
+from oauth2client.service_account import ServiceAccountCredentials
 from bot.core.reporter import rep
 from traceback import format_exc
-from io import BytesIO
 
 def gdrive_auth():
     try:
-        gauth = GoogleAuth()
-
         # Get JSON from Heroku config
         sa_json = os.environ.get("SERVICE_ACCOUNT_JSON")
         if not sa_json:
             raise Exception("❌ SERVICE_ACCOUNT_JSON not set in Heroku Config Vars")
 
-        # Load credentials from JSON string
-        gauth.LoadServiceAccountCredentials(json.loads(sa_json))
+        sa_info = json.loads(sa_json)
+        scopes = ['https://www.googleapis.com/auth/drive']
+        creds = ServiceAccountCredentials.from_json_keyfile_dict(sa_info, scopes)
+        
+        gauth = GoogleAuth()
+        gauth.credentials = creds
         drive = GoogleDrive(gauth)
         return drive
 
