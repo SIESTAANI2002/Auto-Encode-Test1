bc46111c1ab47a4c0a82344e3d8dd965fd589ebb
SIESTAANI2002 <107175442+SIESTAANI2002@users.noreply.github.com>
2025-09-18 14:53:56 +0530

    Update tordownload.py

--- bot/core/tordownload.py
+++ bot/core/tordownload.py
@@ -1,37 +1,41 @@
 from os import path as ospath
 from aiofiles import open as aiopen
 from aiofiles.os import path as aiopath, remove as aioremove, mkdir
-
 from aiohttp import ClientSession
 from torrentp import TorrentDownloader
-from bot import LOGS
 from bot.core.func_utils import handle_logs
 
 class TorDownloader:
-    def __init__(self, path="."):
+    def __init__(self, path="./downloads"):
         self.__downdir = path
         self.__torpath = "torrents/"
-    
+
     @handle_logs
-    async def download(self, torrent, name=None):
-        if torrent.startswith("magnet:"):
-            torp = TorrentDownloader(torrent, self.__downdir)
-            await torp.start_download()
-            return ospath.join(self.__downdir, name)
-        elif torfile := await self.get_torfile(torrent):
-            torp = TorrentDownloader(torfile, self.__downdir)
-            await torp.start_download()
-            await aioremove(torfile)
-            return ospath.join(self.__downdir, torp._torrent_info._info.name())
+    async def download(self, torrent_url, name=None):
+        # Download the .torrent file
+        torfile = await self.get_torfile(torrent_url)
+        if not torfile:
+            return None
+
+        # Use TorrentDownloader
+        torp = TorrentDownloader(torfile, self.__downdir)
+        await torp.start_download()
+        
+        # Remove the .torrent file
+        await aioremove(torfile)
+
+        # Return downloaded file path
+        downloaded_file = ospath.join(self.__downdir, torp._torrent_info._info.name())
+        return downloaded_file if ospath.exists(downloaded_file) else None
 
     @handle_logs
     async def get_torfile(self, url):
         if not await aiopath.isdir(self.__torpath):
             await mkdir(self.__torpath)
-        
+
         tor_name = url.split('/')[-1]
         des_dir = ospath.join(self.__torpath, tor_name)
-        
+
         async with ClientSession() as session:
             async with session.get(url) as response:
                 if response.status == 200:
@@ -40,4 +44,3 @@ class TorDownloader:
                             await file.write(chunk)
                     return des_dir
         return None
-        
