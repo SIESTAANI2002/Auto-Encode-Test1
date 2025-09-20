ef18d20f3f1233911a1c321dab5319f7b206e4f4
SIESTAANI2002 <107175442+SIESTAANI2002@users.noreply.github.com>
2025-09-20 20:29:36 +0530

    Update database.py

--- bot/core/database.py
+++ bot/core/database.py
@@ -1,4 +1,4 @@
-# bot/database.py
+# bot/core/database.py
 from motor.motor_asyncio import AsyncIOMotorClient
 from bot import Var
 
@@ -10,29 +10,33 @@ class MongoDB:
 
     async def getAnime(self, ani_id):
         """Get anime document by ani_id"""
-        botset = await self.animes.find_one({'_id': ani_id})
-        return botset or {}
+        doc = await self.animes.find_one({'_id': ani_id})
+        return doc or {}
 
     async def saveAnime(self, ani_id, ep_no, qual, post_id=None):
         """Mark quality as uploaded for an episode, and save post_id"""
+        ep_no = str(ep_no)
+        # fetch existing episode info
         anime_doc = await self.getAnime(ani_id)
-        ep_info = anime_doc.get(ep_no, {q: False for q in Var.QUALS})
+        episodes = anime_doc.get("episodes", {})
+        ep_info = episodes.get(ep_no, {q: False for q in Var.QUALS})
         ep_info[qual] = True
+        episodes[ep_no] = ep_info
+
+        update_data = {"episodes": episodes}
+        if post_id:
+            update_data["msg_id"] = post_id
+
         await self.animes.update_one(
             {'_id': ani_id},
-            {'$set': {str(ep_no): ep_info}},
+            {'$set': update_data},
             upsert=True
         )
-        if post_id:
-            await self.animes.update_one(
-                {'_id': ani_id},
-                {'$set': {"msg_id": post_id}},
-                upsert=True
-            )
 
     async def getEpisodePost(self, ani_id, ep_no):
         """Return Telegram post_id for a given anime episode"""
         ani_data = await self.getAnime(ani_id)
+        ep_no = str(ep_no)
         post_id = ani_data.get("episodes", {}).get(ep_no, {}).get("post_id")
         if post_id:
             return post_id
@@ -40,6 +44,7 @@ class MongoDB:
 
     async def saveEpisodePost(self, ani_id, ep_no, post_id):
         """Save post_id specifically under episodes dict"""
+        ep_no = str(ep_no)
         await self.animes.update_one(
             {'_id': ani_id},
             {'$set': {f"episodes.{ep_no}.post_id": post_id}},
