ac3e8b869a97acbb2a2a3000aa4803233df31d00
SIESTAANI2002 <107175442+SIESTAANI2002@users.noreply.github.com>
2025-09-21 07:18:41 +0530

    Update database.py

--- bot/core/database.py
+++ bot/core/database.py
@@ -4,8 +4,9 @@ from bot import Var
 
 class MongoDB:
     def __init__(self, uri, database_name):
-        self.client = AsyncIOMotorClient(uri)
+        self.client = AsyncIOMotorClient(uri or Var.MONGO_URI)
         self.db = self.client[database_name]
+        # main collection keyed by bot token prefix
         self.animes = self.db.animes[Var.BOT_TOKEN.split(':')[0]]
 
     async def getAnime(self, ani_id):
@@ -14,35 +15,30 @@ class MongoDB:
         return anime_doc or {}
 
     async def saveAnime(self, ani_id, ep_no, qual, post_id=None):
-        """Mark a quality as uploaded for an episode, and save post_id"""
+        """Mark quality as uploaded and store post_id"""
         anime_doc = await self.getAnime(ani_id)
         episodes = anime_doc.get("episodes", {})
 
-        # update episode info
         ep_info = episodes.get(str(ep_no), {q: False for q in Var.QUALS})
         ep_info[qual] = True
-
-        # save post_id if not already set
-        if post_id:
-            ep_info["post_id"] = post_id
-
         episodes[str(ep_no)] = ep_info
 
-        await self.animes.update_one(
-            {'_id': ani_id},
-            {'$set': {"episodes": episodes}},
-            upsert=True
-        )
+        update_data = {"episodes": episodes}
+        if post_id:
+            update_data["msg_id"] = post_id  # global fallback for post
+
+        await self.animes.update_one({'_id': ani_id}, {'$set': update_data}, upsert=True)
 
     async def getEpisodePost(self, ani_id, ep_no):
-        """Return Telegram post_id for a given anime episode"""
-        anime_doc = await self.getAnime(ani_id)
-        episodes = anime_doc.get("episodes", {})
-        ep_info = episodes.get(str(ep_no), {})
-        return ep_info.get("post_id")
+        """Return Telegram post_id for a given episode"""
+        ani_data = await self.getAnime(ani_id)
+        post_id = ani_data.get("episodes", {}).get(str(ep_no), {}).get("post_id")
+        if post_id:
+            return post_id
+        return ani_data.get("msg_id")  # fallback global post_id
 
     async def saveEpisodePost(self, ani_id, ep_no, post_id):
-        """Save post_id specifically under episodes dict"""
+        """Save post_id under episodes dict"""
         await self.animes.update_one(
             {'_id': ani_id},
             {'$set': {f"episodes.{ep_no}.post_id": post_id}},
@@ -54,4 +50,5 @@ class MongoDB:
         await self.animes.drop()
 
 
+# instance for use
 db = MongoDB(Var.MONGO_URI, "FZAutoAnimes")
