8e4b19c00ee5e21829f03893c2c9fc16c0404133
SIESTAANI2002 <107175442+SIESTAANI2002@users.noreply.github.com>
2025-09-30 16:57:05 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -8,7 +8,7 @@ from sys import executable
 from signal import SIGKILL
 
 from bot import bot, Var, bot_loop, sch, LOGS, ffQueue, ffLock, ffpids_cache, ff_queued
-from bot.core.auto_animes import fetch_animes, handle_file_click
+from bot.core.auto_animes import fetch_animes
 from bot.core.func_utils import clean_up, new_task, editMessage
 from bot.modules.up_posts import upcoming_animes
 
